f4e069dad71d1e4cc93bfaf6b273d346635180e1
ExKaSeT <exkaset@mail.ru>
2025-03-06 22:53:04 +0300

    kubeconfig to file

--- src/main/java/last/project/jvmtuner/config/KubernetesClientConfig.java
+++ src/main/java/last/project/jvmtuner/config/KubernetesClientConfig.java
@@ -1,15 +1,28 @@
 package last.project.jvmtuner.config;
 
+import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Configuration
 public class KubernetesClientConfig {
 
+    @Value("${k8s-client.config-path}")
+    private String configPath;
+
     @Bean
+    @SneakyThrows
     public KubernetesClient getK8sClient() {
-        return new KubernetesClientBuilder().build();
+        String config = Files.readString(Paths.get(configPath));
+        return new KubernetesClientBuilder()
+                .withConfig(Config.fromKubeconfig(config))
+                .build();
     }
 }
--- src/test/java/last/project/jvmtuner/util/GetBaseData.java
+++ src/test/java/last/project/jvmtuner/util/GetBaseData.java
@@ -74,7 +74,7 @@ public class GetBaseData {
         return service.saveTuningTestProps(deployment, "crypto",
                 "8080/actuator/prometheus", "exkaset/gatling:1.0",
                 "bash -c \"mvn gatling:test > /dev/null 2> /dev/null &\"",
-                90, 400,
+                90, 300,
                 List.of(new MetricMaxValueDto()
                         .setQuery("sum(gatling_count_total{type=\"ko\", $jvm_tuner_id}) / sum(gatling_count_total{type=\"ok\", $jvm_tuner_id}) * 100")
                         .setMaxValue(10L)), null);
